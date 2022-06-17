ebda75c89d1aeb89f90951e26fa80399ef24fb3a
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-06-18 01:01:09 +0300

    Revert "Changing string to Uppercase"
    This reverts commit 90364934914d0a210ea8e68c878b7dc9bde68649.

--- Sequences/buycomputer.py
+++ Sequences/buycomputer.py
@@ -5,26 +5,26 @@ while current_choice != '0':
     if current_choice in "123456" and current_choice not in "":
         print("Adding {}.".format(current_choice))
         if current_choice == '1':
-            computer_parts.append("Computer")
+            computer_parts.append("computer")
         elif current_choice == '2':
-            computer_parts.append("Monitor")
+            computer_parts.append("monitor")
         elif current_choice == '3':
-            computer_parts.append("Keyboard")
+            computer_parts.append("keyboard")
         elif current_choice == '4':
-            computer_parts.append("Mouse")
+            computer_parts.append("mouse")
         elif current_choice == '5':
-            computer_parts.append("Mouse mat")
+            computer_parts.append("mouse mat")
         elif current_choice == '6':
-            computer_parts.append("HDMI cable")
+            computer_parts.append("hdmi cable")
     else:
         print("Please add options from the list below: ")
-        print("1: Computer")
-        print("2: Monitor")
-        print("3: Keyboard")
-        print("4: Mouse")
-        print("5: Mouse mat")
-        print("6: HDMI cable")
-        print("0: Finish")
+        print("1: computer")
+        print("2: monitor")
+        print("3: keyboard")
+        print("4: mouse")
+        print("5: mouse mat")
+        print("6: hdmi cable")
+        print("0: finish")
 
     current_choice = input()
 
