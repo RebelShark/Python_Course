90364934914d0a210ea8e68c878b7dc9bde68649
nrusev93 <115-knz@unibit.bg>
2022-06-18 00:51:19 +0300

    Changing string to Uppercase

--- Sequences/buycomputer.py
+++ Sequences/buycomputer.py
@@ -5,26 +5,26 @@ while current_choice != '0':
     if current_choice in "123456" and current_choice not in "":
         print("Adding {}.".format(current_choice))
         if current_choice == '1':
-            computer_parts.append("computer")
+            computer_parts.append("Computer")
         elif current_choice == '2':
-            computer_parts.append("monitor")
+            computer_parts.append("Monitor")
         elif current_choice == '3':
-            computer_parts.append("keyboard")
+            computer_parts.append("Keyboard")
         elif current_choice == '4':
-            computer_parts.append("mouse")
+            computer_parts.append("Mouse")
         elif current_choice == '5':
-            computer_parts.append("mouse mat")
+            computer_parts.append("Mouse mat")
         elif current_choice == '6':
-            computer_parts.append("hdmi cable")
+            computer_parts.append("HDMI cable")
     else:
         print("Please add options from the list below: ")
-        print("1: computer")
-        print("2: monitor")
-        print("3: keyboard")
-        print("4: mouse")
-        print("5: mouse mat")
-        print("6: hdmi cable")
-        print("0: finish")
+        print("1: Computer")
+        print("2: Monitor")
+        print("3: Keyboard")
+        print("4: Mouse")
+        print("5: Mouse mat")
+        print("6: HDMI cable")
+        print("0: Finish")
 
     current_choice = input()
 
