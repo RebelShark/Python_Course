370c2eb5ddea244717f1fbc4807ddbb410d4d9cc
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-06-20 16:02:34 +0300

    x

--- Sequences/Test.py
+++ Sequences/Test.py
@@ -1,10 +1,11 @@
-available_parts = ["computer",
-                   "monitor",
-                   "keyboard",
-                   "mouse",
-                   "hdmi cable",
-                   "dvd drive"
-                   ]
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
 
-for i, n in enumerate(available_parts):
-    print("{}: {}".format(i + 1, n))
+min_valid = 100
+max_valid = 200
+
+for index in range(len(data) - 1, - 1, -1):
+        if data[index] < min_valid or data[index] > max_valid:
+                print(index, data)
+                del data[index]
+print(data)
---
+++ Sequences/joining_things.py
@@ -0,0 +1,12 @@
+flowers = [
+    "Daffodil",
+    "Evening Primrose",
+    "Hydrangea",
+    "Iris",
+    "Lavender",
+    "Sunflower",
+    "Tiger Lily",
+]
+
+for flower in flowers:
+    print(flower)
